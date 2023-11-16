a59d575b1c761aba7615cde20beb432f9452f41a
GitHub Action <action@github.com>
2023-11-16 08:45:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/dengliwendecode/template/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/dengliwendecode/template/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dengliwendecode.template.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
